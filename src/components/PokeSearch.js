e29525be12c10aec0488466aa7f58daef3582469
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-11-25 17:08:13 -0800

    added starter files

---
+++ src/components/PokeSearch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// function to fetch data from the apis and then make the pokemon card, offensive card, and defensive card
+
+export default function Home() {
+  return (
+    <div>
+      <h1>I'm the poke search </h1>
+      {/* form - */}
+      {/* the search bar label "what's your pokemon" */}
+      {/* the search bar (input) */}
+   
+    </div>
+  );
+}
