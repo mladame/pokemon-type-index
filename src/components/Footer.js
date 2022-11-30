195325de66a51de2f4a70223ff4050414e3bdc1b
Stevo <steven.barrios92@gmail.com>
2022-11-30 13:30:23 -0800

    footer and icon for it, with css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import '../../src/index.css'
 
 export default function Footer() {
   return (
-    <div>
-      <h1>I'm the Footer </h1>
-   
+    <div className='footer'>
+      &copy; {new Date().getFullYear()} <i className="fa-brands fa-github custom-icon fa-xl"></i>
     </div>
   );
 }
