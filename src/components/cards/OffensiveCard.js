c5dcfb58265870e39e05ded5e863cc72c982fdb0
cindy <cindyung56@gmail.com>
2022-12-01 15:50:05 -0800

    added code to filter out duplicates

--- src/components/cards/OffensiveCard.js
+++ src/components/cards/OffensiveCard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export default function OffensiveCard({ pokeTypes }) {
-  console.log(pokeTypes);
+  // console.log(pokeTypes);
   const [deals2x, setDeals2x] = useState([]);
   const [dealsHalf, setDealsHalf] = useState([]);
   const [dealsNone, setDealsNone] = useState([]);
@@ -20,23 +20,59 @@ export default function OffensiveCard({ pokeTypes }) {
           return response.json();
         })
         .then(function (data) {
-          console.log(data);
+          // console.log(data);
 
           deals2xBucket.push(...data.damage_relations.double_damage_to);
           dealsHalfBucket.push(...data.damage_relations.half_damage_to);
           dealsNoneBucket.push(...data.damage_relations.no_damage_to);
+
 
           fetch(typeURL2)
             .then(function (response2) {
               return response2.json();
             })
             .then(function (data2) {
-              console.log(data2);
+              // console.log(data2);
 
               // ! todo : make sure that it does not repeat what's in the array already
-              deals2xBucket.push(...data2.damage_relations.double_damage_to);
-              dealsHalfBucket.push(...data2.damage_relations.half_damage_to);
-              dealsNoneBucket.push(...data2.damage_relations.no_damage_to);
+              // (kinda?) solved: it at least filters out the same type from the same section
+              // might need to discuss what to do about types that appear in multiple sections (search Torterra or Empoleon for an example)
+              const type2Deals2x = data2.damage_relations.double_damage_to;
+              const doubleNames = [];
+              for(var j = 0; j < deals2xBucket.length; j++){
+                doubleNames.push(deals2xBucket[j].name);
+              }
+              for (var i = 0; i < type2Deals2x.length; i++){
+                if (!(doubleNames.includes(type2Deals2x[i].name))){
+                  deals2xBucket.push(type2Deals2x[i]);
+                }
+              }
+
+              const type2DealsHalf = data2.damage_relations.half_damage_to;
+              const halfNames = [];
+              for(var j = 0; j < dealsHalfBucket.length; j++){
+                halfNames.push(dealsHalfBucket[j].name);
+              }
+              for (var i = 0; i < type2DealsHalf.length; i++){
+                if (!(halfNames.includes(type2DealsHalf[i].name))){
+                  dealsHalfBucket.push(type2DealsHalf[i]);
+                }
+              }
+
+              const type2DealsNone = data2.damage_relations.no_damage_to;
+              const noDmgNames = [];
+              for(var j = 0; j < dealsNoneBucket.length; j++){
+                noDmgNames.push(dealsNoneBucket[j].name);
+              }
+              for (var i = 0; i < type2DealsNone.length; i++){
+                if (!(noDmgNames.includes(type2DealsNone[i].name))){
+                  dealsNoneBucket.push(type2DealsNone[i]);
+                }
+              }
+
+              // deals2xBucket.push(...data2.damage_relations.double_damage_to);
+              // dealsHalfBucket.push(...data2.damage_relations.half_damage_to);
+              // dealsNoneBucket.push(...data2.damage_relations.no_damage_to);
 
               setDeals2x(deals2xBucket);
               setDealsHalf(dealsHalfBucket);
@@ -52,7 +88,7 @@ export default function OffensiveCard({ pokeTypes }) {
           return response.json();
         })
         .then(function (data) {
-          console.log(data);
+          // console.log(data);
           setDeals2x(data.damage_relations.double_damage_to);
           setDealsHalf(data.damage_relations.half_damage_to);
           setDealsNone(data.damage_relations.no_damage_to);
@@ -60,7 +96,7 @@ export default function OffensiveCard({ pokeTypes }) {
     }
   }, [pokeTypes]);
 
-  console.log(deals2x);
+  // console.log(deals2x);
 
   return (
     <div className="pokeCard col-6 col-md-12 col-lg-6">
