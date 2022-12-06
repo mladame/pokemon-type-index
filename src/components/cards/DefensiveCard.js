c2c259b6e24b833b3bfc20569d6b996faad5262d
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-12-06 12:30:30 -0800

    working on x2, no longer uses type of pokemon in the no damage

--- src/components/cards/DefensiveCard.js
+++ src/components/cards/DefensiveCard.js
@@ -8,7 +8,6 @@ export default function DefensiveCard({ pokeTypes }) {
   const [takes4x, setTakes4x] = useState([]);
   const [takesQuarter, setTakesQuarter] = useState([]);
 
-
   useEffect(() => {
     // if a pokemon has two types, it'll fetch twice
     if (pokeTypes.length > 1) {
@@ -19,6 +18,9 @@ export default function DefensiveCard({ pokeTypes }) {
       const takesNoneBucket = [];
       const times4xBucket = [];
       const Quarter = [];
+
+      const type1 = pokeTypes[0];
+      const type2 = pokeTypes[1];
 
       fetch(typeURL)
         .then(function (response) {
@@ -36,62 +38,76 @@ export default function DefensiveCard({ pokeTypes }) {
               return response2.json();
             })
             .then(function (data2) {
-              const type2takes2x = data2.damage_relations.double_damage_from;
-              const doubleNames = [];
-              for (var j = 0; j < takes2xBucket.length; j++) {
-                doubleNames.push(takes2xBucket[j].name);
-              }
-
-
-              // for (var i = 0; i < type2takes2x.length; i++) {
-              //   if (!(doubleNames.includes(type2takes2x[i].name))) {
-              //     takes2xBucket.push(type2takes2x[i]);
-              //   } else {
-              //     times4xBucket.push(type2takes2x[i]);
-              //   }
-              // }
-
-              for (var i = 0; i < type2takes2x.length; i++) {
-                if ((doubleNames.includes(type2takes2x[i].name))) {
-                  times4xBucket.push(type2takes2x[i]);
-                }
-                 else if(times4xBucket.includes(type2takes2x[i].name)) {
-                  takes2xBucket.concat(type2takes2x[i]);
-                }
-                
-              }
-
-
-
-
-              const types2Takeshalf = data2.damage_relations.half_damage_from;
-              const halfNames = [];
-              for (var j = 0; j < takesHalfBucket.length; j++) {
-                halfNames.push(takesHalfBucket[j].name);
-              }
-              for (var i = 0; i < types2Takeshalf.length; i++) {
-                if (!(halfNames.includes(types2Takeshalf[i].name))) {
-                  takesHalfBucket.push(types2Takeshalf[i]);
-                } else {
-                  Quarter.push(types2Takeshalf[i]);
-                }
-              }
-
+              // this is for the takes no damage
               const types2TakesNone = data2.damage_relations.no_damage_from;
               const noDmgNames = [];
               for (var j = 0; j < takesNoneBucket.length; j++) {
                 noDmgNames.push(takesNoneBucket[j].name);
               }
               for (var i = 0; i < types2TakesNone.length; i++) {
-                if (!(noDmgNames.includes(types2TakesNone[i].name))) {
+                if (!noDmgNames.includes(types2TakesNone[i].name)) {
                   takesNoneBucket.push(types2TakesNone[i]);
                 }
               }
 
-              // deals2xBucket.push(...data2.damage_relations.double_damage_to);
-              // dealsHalfBucket.push(...data2.damage_relations.half_damage_to);
-              // types2TakesNone.push(...data2.damage_relations.no_damage_to);
+              // this is for the takes 1/2x damage and takes 1/4x damage
+              const types2Takeshalf = data2.damage_relations.half_damage_from;
+              const halfNames = [];
+              for (var j = 0; j < takesHalfBucket.length; j++) {
+                halfNames.push(takesHalfBucket[j].name);
+              }
+              for (var i = 0; i < types2Takeshalf.length; i++) {
+                if (
+                  !halfNames.includes(types2Takeshalf[i].name) &&
+                  !(types2Takeshalf[i].name === type1) &&
+                  !(types2Takeshalf[i].name === type2)
+                ) {
+                  takesHalfBucket.push(types2Takeshalf[i]);
+                } else {
+                  Quarter.push(types2Takeshalf[i]);
+                }
+              }
 
+              // this is for the takes 2x damage and takes 4x damage
+              const type2takes2x = data2.damage_relations.double_damage_from;
+              const doubleNames = [];
+
+              for (var j = 0; j < takes2xBucket.length; j++) {
+                doubleNames.push(takes2xBucket[j].name);
+              }
+
+              for (var i = 0; i < type2takes2x.length; i++) {
+                if (doubleNames.includes(type2takes2x[i].name)) {
+                  times4xBucket.push(type2takes2x[i]);
+                }
+
+                console.log(times4xBucket);
+
+                function example() {
+                  return times4xBucket.some((e) => e.name === type2takes2x[i].name);
+                }
+
+                console.log(type2takes2x);
+
+                console.log(example())
+
+                if (
+                  !doubleNames.includes(type2takes2x[i].name) &&
+                  !(type2takes2x[i].name === type1) &&
+                  !(type2takes2x[i].name === type2) &&
+                  !times4xBucket.some((e) => e.name === type2takes2x[i].name) 
+                  
+                  // not in the takes none bucket
+                  // not in takes x4 bucket
+                ) {
+                  takes2xBucket.push(type2takes2x[i]);
+                }
+              }
+
+              console.log(times4xBucket);
+              console.log(takes2xBucket);
+
+              // setting the states
               setTakes2x(takes2xBucket);
               setTakesHalf(takesHalfBucket);
               setTakesNone(takesNoneBucket);
@@ -123,31 +139,41 @@ export default function DefensiveCard({ pokeTypes }) {
       <h3>Takes 4x Damage from : </h3>
       <ul>
         {takes4x.map((type) => (
-          <li className={`poke-type ${type.name}-type`}>{type.name.toUpperCase()}</li>
+          <li className={`poke-type ${type.name}-type`}>
+            {type.name.toUpperCase()}
+          </li>
         ))}
       </ul>
       <h3>Takes 2x Damage from : </h3>
       <ul>
         {takes2x.map((type) => (
-          <li className={`poke-type ${type.name}-type`}>{type.name.toUpperCase()}</li>
+          <li className={`poke-type ${type.name}-type`}>
+            {type.name.toUpperCase()}
+          </li>
         ))}
       </ul>
       <h3>Takes 1/2x Damage from : </h3>
       <ul>
         {takesHalf.map((type) => (
-          <li className={`poke-type ${type.name}-type`}>{type.name.toUpperCase()}</li>
-        ))}
-      </ul>
-      <h3>Takes No Damage from : </h3>
-      <ul>
-        {takesNone.map((type) => (
-          <li className={`poke-type ${type.name}-type`}>{type.name.toUpperCase()}</li>
+          <li className={`poke-type ${type.name}-type`}>
+            {type.name.toUpperCase()}
+          </li>
         ))}
       </ul>
       <h3>Takes 1/4x Damage from : </h3>
       <ul>
         {takesQuarter.map((type) => (
-          <li className={`poke-type ${type.name}-type`}>{type.name.toUpperCase()}</li>
+          <li className={`poke-type ${type.name}-type`}>
+            {type.name.toUpperCase()}
+          </li>
+        ))}
+      </ul>
+      <h3>Takes No Damage from : </h3>
+      <ul>
+        {takesNone.map((type) => (
+          <li className={`poke-type ${type.name}-type`}>
+            {type.name.toUpperCase()}
+          </li>
         ))}
       </ul>
     </div>
