68095c84b053bd575dc062a3a22139b5a1f203b4
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-12-22 23:02:49 -0800

    attempting a new way

--- src/components/cards/DefensiveCard.js
+++ src/components/cards/DefensiveCard.js
@@ -22,99 +22,130 @@ export default function DefensiveCard({ pokeTypes }) {
       const type1 = pokeTypes[0];
       const type2 = pokeTypes[1];
 
+      let combineData = [];
+      
+
       fetch(typeURL)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
-          // console.log(data);
-
-          takes2xBucket.push(...data.damage_relations.double_damage_from);
-          takesHalfBucket.push(...data.damage_relations.half_damage_from);
-          takesNoneBucket.push(...data.damage_relations.no_damage_from);
+          combineData.push(data)
 
           fetch(typeURL2)
             .then(function (response2) {
               return response2.json();
             })
             .then(function (data2) {
-              // this is for the takes no damage
-              const types2TakesNone = data2.damage_relations.no_damage_from;
-              const noDmgNames = [];
-              for (var j = 0; j < takesNoneBucket.length; j++) {
-                noDmgNames.push(takesNoneBucket[j].name);
-              }
-              for (var i = 0; i < types2TakesNone.length; i++) {
-                if (!noDmgNames.includes(types2TakesNone[i].name)) {
-                  takesNoneBucket.push(types2TakesNone[i]);
-                }
-              }
-
-              // this is for the takes 1/2x damage and takes 1/4x damage
-              const types2Takeshalf = data2.damage_relations.half_damage_from;
-              const halfNames = [];
-              for (var j = 0; j < takesHalfBucket.length; j++) {
-                halfNames.push(takesHalfBucket[j].name);
-              }
-              for (var i = 0; i < types2Takeshalf.length; i++) {
-                if (
-                  !halfNames.includes(types2Takeshalf[i].name) &&
-                  !(types2Takeshalf[i].name === type1) &&
-                  !(types2Takeshalf[i].name === type2)
-                ) {
-                  takesHalfBucket.push(types2Takeshalf[i]);
-                } else {
-                  Quarter.push(types2Takeshalf[i]);
-                }
-              }
-
-              // this is for the takes 2x damage and takes 4x damage
-              const type2takes2x = data2.damage_relations.double_damage_from;
-              const doubleNames = [];
-
-              for (var j = 0; j < takes2xBucket.length; j++) {
-                doubleNames.push(takes2xBucket[j].name);
-              }
-
-              for (var i = 0; i < type2takes2x.length; i++) {
-                if (doubleNames.includes(type2takes2x[i].name)) {
-                  times4xBucket.push(type2takes2x[i]);
-                }
-
-                console.log(times4xBucket);
-
-                function example() {
-                  return times4xBucket.some((e) => e.name === type2takes2x[i].name);
-                }
-
-                console.log(type2takes2x);
-
-                console.log(example())
-
-                if (
-                  !doubleNames.includes(type2takes2x[i].name) &&
-                  !(type2takes2x[i].name === type1) &&
-                  !(type2takes2x[i].name === type2) &&
-                  !times4xBucket.some((e) => e.name === type2takes2x[i].name) 
-                  
-                  // not in the takes none bucket
-                  // not in takes x4 bucket
-                ) {
-                  takes2xBucket.push(type2takes2x[i]);
-                }
-              }
-
-              console.log(times4xBucket);
-              console.log(takes2xBucket);
-
-              // setting the states
-              setTakes2x(takes2xBucket);
-              setTakesHalf(takesHalfBucket);
-              setTakesNone(takesNoneBucket);
-              setTakes4x(times4xBucket);
-              setTakesQuarter(Quarter);
+              combineData.push(data2)
             });
-        });
+      });
+
+      console.log(combineData);
+
+      //! this is for the takes no damage
+      
+
+      //! this is for the takes 1/2x damage and takes 1/4x damage
+
+      //! this is for the takes 2x damage and takes 4x damage
+
+      // ------------------------------------------------------------------------------------------------//
+      // fetch(typeURL)
+      //   .then(function (response) {
+      //     return response.json();
+      //   })
+      //   .then(function (data) {
+      //     // console.log(data);
+
+      //     takes2xBucket.push(...data.damage_relations.double_damage_from);
+      //     takesHalfBucket.push(...data.damage_relations.half_damage_from);
+      //     takesNoneBucket.push(...data.damage_relations.no_damage_from);
+
+      //     fetch(typeURL2)
+      //       .then(function (response2) {
+      //         return response2.json();
+      //       })
+      //       .then(function (data2) {
+      //         // this is for the takes no damage
+      //         const types2TakesNone = data2.damage_relations.no_damage_from;
+      //         const noDmgNames = [];
+      //         for (var j = 0; j < takesNoneBucket.length; j++) {
+      //           noDmgNames.push(takesNoneBucket[j].name);
+      //         }
+      //         for (var i = 0; i < types2TakesNone.length; i++) {
+      //           if (!noDmgNames.includes(types2TakesNone[i].name)) {
+      //             takesNoneBucket.push(types2TakesNone[i]);
+      //           }
+      //         }
+
+      //         // this is for the takes 1/2x damage and takes 1/4x damage
+      //         const types2Takeshalf = data2.damage_relations.half_damage_from;
+      //         const halfNames = [];
+      //         for (var j = 0; j < takesHalfBucket.length; j++) {
+      //           halfNames.push(takesHalfBucket[j].name);
+      //         }
+      //         for (var i = 0; i < types2Takeshalf.length; i++) {
+      //           if (
+      //             !halfNames.includes(types2Takeshalf[i].name) &&
+      //             !(types2Takeshalf[i].name === type1) &&
+      //             !(types2Takeshalf[i].name === type2)
+      //           ) {
+      //             takesHalfBucket.push(types2Takeshalf[i]);
+      //           } else {
+      //             Quarter.push(types2Takeshalf[i]);
+      //           }
+      //         }
+
+      //         // this is for the takes 2x damage and takes 4x damage
+      //         const type2takes2x = data2.damage_relations.double_damage_from;
+      //         const doubleNames = [];
+
+      //         for (var j = 0; j < takes2xBucket.length; j++) {
+      //           doubleNames.push(takes2xBucket[j].name);
+      //         }
+
+      //         for (var i = 0; i < type2takes2x.length; i++) {
+      //           if (doubleNames.includes(type2takes2x[i].name)) {
+      //             times4xBucket.push(type2takes2x[i]);
+      //           }
+
+      //           console.log(times4xBucket);
+
+      //           function example() {
+      //             return times4xBucket.some((e) => e.name === type2takes2x[i].name);
+      //           }
+
+      //           console.log(type2takes2x);
+
+      //           console.log(example())
+
+      //           if (
+      //             !doubleNames.includes(type2takes2x[i].name) &&
+      //             !(type2takes2x[i].name === type1) &&
+      //             !(type2takes2x[i].name === type2) &&
+      //             !times4xBucket.some((e) => e.name === type2takes2x[i].name)
+
+      //             // not in the takes none bucket
+      //             // not in takes x4 bucket
+      //           ) {
+      //             takes2xBucket.push(type2takes2x[i]);
+      //           }
+      //         }
+
+      //         console.log(times4xBucket);
+      //         console.log(takes2xBucket);
+
+      //         // setting the states
+      //         setTakes2x(takes2xBucket);
+      //         setTakesHalf(takesHalfBucket);
+      //         setTakesNone(takesNoneBucket);
+      //         setTakes4x(times4xBucket);
+      //         setTakesQuarter(Quarter);
+      //       });
+      //   });
+      // ------------------------------------------------------------------------------------------------//
+
     } else {
       // if the pokemon only has one type it'll fetch once
       const typeURL = `https://pokeapi.co/api/v2/type/${pokeTypes}/`;
